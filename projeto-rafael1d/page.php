334b34d74be468009ead88d5517ae899791082bd
Rafa777x <rafael.moreira25@escola.pr.gov.br>
2025-10-06 11:12:15 -0300

    Add files via upload

---
+++ projeto-rafael1d/page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Formulario</title>
+</head>
+<body>
+
+<form style="padding: 20px; border: 1px solid #c70000; width: 250px; margin: 50px auto; font-family: Arial, sans-serif;">
+  <input 
+    type="text" 
+    name="nome" 
+    placeholder="Nome" 
+    style="padding: 8px; width: 100%; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #999; border-radius: 4px;"
+  >
+  <input 
+  type="password"
+  name="senha"
+  placeholder="Senha"
+  style="padding: 8px; width: 100%; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #999; border-radius: 4px;"
+  >
+
+  <input 
+    type="submit" 
+    value="Enviar" 
+    style="padding: 8px; width: 100%; background-color: #c70000; border: none; color: white; border-radius: 4px; cursor: pointer;"
+  >
+</form>
+
+</body>
+</html>
